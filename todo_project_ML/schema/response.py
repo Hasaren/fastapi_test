f25f4fb514936bc2c704eb4565d23812597e73ec
Hasaren <1rlaskgus@naver.com>
2026-08-20 12:47:49 +0900

    Todo project ML 추가

--- todo_project_ML/schema/response.py
+++ todo_project_ML/schema/response.py
@@ -14,6 +14,9 @@ class TodoResponse(BaseModel):
     is_done: bool
     # 이 할일이 누구의 것인지 를 클라이언트가 화면에서 구분할 수 있도록 추가
     user_id: int | None
+    # --------
+    predicted_category: str | None = None
+    final_category: str | None = None
 
 # 회원가입 응답 모델
 # Pydantic이 password를 필터링 시켜 제외
